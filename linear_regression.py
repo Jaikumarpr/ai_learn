d192d15b9e898047f619c1cbf657e3d34e3c4bda
Jaikumar Pettikkattil <jaikumar@jaikumars-mbp.attlocal.net>
2017-11-22 13:30:44 -0800

    added batch gradient descent

--- linear_regression.py
+++ linear_regression.py
@@ -34,8 +34,39 @@ def linear_cost(features, params, train_out, train_size=None):
     return 0.5 * np.sum(squared_error(features, params, train_out)) / train_size
 
 
-def batch_grad_descent():
-    pass
+# check for tolerance
+def under_tolerance(temp, val, tol):
+    return np.sum(np.abs(temp - val)) <= tol
+
+
+# batch gradienct descent for linear regression
+def batch_grad_descent(features, params, train_out, train_size,
+                       tolerance=None):
+    if tolerance is None:
+        tolerance = 0.0001
+
+    learn_rate = 0.6
+    cost_function_array = []
+    params_array = np.array([])
+    temp_params = np.zeros(features.shape[1])
+
+    while True:
+
+        for i in range(params.shape[0]):
+            grd = gradient(features, params, train_out, train_size)
+            feature_i = features[:, i]
+            temp_params[i] = params[i] - (learn_rate * np.sum(grd * feature_i))
+
+        params = np.copy(temp_params)
+
+        cost_function_array.append(linear_cost(features, params, train_out,
+                                               train_size))
+        params_array.append(params)
+
+        if under_tolerance(temp_params, params, tolerance):
+            break
+
+    return params, cost_function_array, params_array
 
 
 def stochastic_grad_descent():
